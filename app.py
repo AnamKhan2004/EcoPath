8a5cb9ee679b1d5907f6c27c02cd9def02d7ef17
AnamKhan2004 <anamykhan@gmail.com>
2024-07-27 16:22:02 -0400

    basic setup of web app

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
-# Create an instance of the Flask class
 app = Flask(__name__)
 
-# Define a route for the root URL
 @app.route('/')
-def home():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
 
-# Define another route that returns JSON data
-@app.route('/api')
-def api():
-    data = {"message": "Welcome to the Flask API"}
-    return jsonify(data)
-
-# Run the application if this script is executed directly
 if __name__ == '__main__':
     app.run(debug=True)
